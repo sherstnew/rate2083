6d754c28c45d544f8b8ec9e5f4910ceeda2ecfef
denis <88676144+sherstnew@users.noreply.github.com>
2022-08-18 11:52:19 +0300

    working

--- src/api.js
+++ src/api.js
@@ -1,11 +1,31 @@
 const express = require('express')
+const sqlite3 = require('sqlite3').verbose()
 const serverless = require("serverless-http");
 
 const app = express()
 const router = express.Router()
+const db = new sqlite3.Database('db/teachers.db')
 
-router.get('/', (req, res) => {
-    res.send('hi!')
+const query = (command, method = 'all') => {
+    return new Promise((resolve, reject) => {
+      db[method](command, (error, result) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(result);
+        }
+      });
+    });
+  };
+
+
+router.post('/handler', (req, res) => {
+    data = req.body
+    db.serialize(async () => {
+        let sel = await query('SELECT * FROM teachers');
+        res.send(sel)
+      });
+    res.send('ok');
 })
 
 app.use(express.json())
