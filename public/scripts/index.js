752c92eaeb266f933791dc2c2717a343b32abc3f
denis <88676144+sherstnew@users.noreply.github.com>
2022-08-20 11:19:53 +0000

    work in progress

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,31 +1,56 @@
 const tphoto = document.querySelector('.card-photo')
 const tname = document.querySelector('.desc-name')
 const tjob = document.querySelector('.desc-job')
-let randteacher = {}
+const tlikes = document.querySelector('.rating-counter')
+const change_btn = document.querySelector('.change-btn')
+const like_btn = document.querySelector('.uil-thumbs-up')
+let trand = 0
+let teacher = {}
+let localst = ''
+let likes = ''
+let teachers = []
 
-const teacher = new XMLHttpRequest()
+const change = () => {
+    const req = new XMLHttpRequest()
+    
 
-teacher.open('POST', '/api', true)
-
-let body = {
-  query: 'teachers'
-}
-
-body = JSON.stringify(body)
-
-teacher.setRequestHeader('Content-Type', 'application/json')
-
-teacher.send(body)
-
-teacher.onload = () => {
-    if (teacher.responseText == 'all') {
-        tphoto.style.backgroundImage = "url('/img/allphoto.png')"
-        tname.innerHTML = 'Все!'
-        tjob.innerHTML = 'Вы оценили всех учителей, спасибо!'
-    } else {
-        randteacher = JSON.parse(teacher.responseText)
-        tphoto.style.backgroundImage = "url('" + randteacher.photo + "')"
-        tname.innerHTML = randteacher.name
-        tjob.innerHTML = randteacher.job
+    req.open('POST', '/api', true)
+    let body = {
+      q: "teachers"
+    }
+    body = JSON.stringify(body)
+    req.setRequestHeader('Content-Type', 'application/json')
+    req.send(body)
+    
+    req.onload = () => {
+        teachers = JSON.parse(req.response)
+        trand = Math.floor(Math.random() * teachers.length)
+        teacher = teachers[trand]
+        tphoto.style.backgroundImage = "url('" + teacher.photo + "')"
+        tname.innerHTML = teacher.name
+        tjob.innerHTML = teacher.job
+        tlikes.innerHTML = teacher.likes
+        teachers.splice(trand, 1)
     }
 }
+
+change()
+
+change_btn.addEventListener('click', change)
+
+like_btn.addEventListener('click', () => {
+    likes++
+    const req_like = new XMLHttpRequest()
+    req_like.open('POST', '/api', true)
+    let body = {
+      name: "",
+      likes: 0,
+      q: "like"
+    }
+    body.name = teacher.name
+    body.likes = likes
+    body = JSON.stringify(body)
+    req_like.setRequestHeader('Content-Type', 'application/json')
+    req_like.send(body)
+    change()
+})
--- server.js
+++ server.js
@@ -2,13 +2,41 @@ const express = require('express')
 const sqlite3 = require('sqlite3')
 
 const app = express()
-const db = new sqlite3.Database('teachers.db')
 let data = ''
 let randid = 0
 let sel = []
+let db
 
 app.use(express.json())
 app.use(express.static('public'))
+
+// routes
+
+app.get('/', (req, res) => {
+    res.sendFile('/home/runner/rate2083/public/pages/index.html')
+})
+app.post('/api', (req, res) => {
+    q = req.body.q
+    if (q == 'teachers') {
+        db = new sqlite3.Database('teachers.db')
+        db.serialize(async () => {
+          sel = await query('SELECT * FROM teachers')
+        })
+        db.close()
+        res.send(sel)
+    } else if (q == 'like') {
+        db = new sqlite3.Database('teachers.db')
+        let name = req.body.name
+        let likes = req.body.likes
+        db.serialize(() => {
+            let update = 'UPDATE teachers SET likes=' + likes + ' WHERE name="' + name + '"'
+            db.run(update)
+            
+        })
+        db.close()
+        res.send('ok')
+    }
+})
 
 const query = (command, method = 'all') => {
   return new Promise((resolve, reject) => {
@@ -22,31 +50,5 @@ const query = (command, method = 'all') => {
   });
 };
 
-db.serialize(async () => {
-  sel = await query('SELECT * FROM teachers')
-})
 
-const teach = (req, res) => {
-  data = req.body.query
-  if (data == 'teachers') {
-    
-    randid = Math.floor(Math.random() * sel.length)
-    console.log(sel, randid)
-
-    if (sel.length === 0) {
-      res.send('all')
-    } else {
-      res.send(sel[randid])
-      sel.splice(randid, 1)
-    }
-  }
-}
-
-app.get('/', (req, res) => {
-    res.sendFile('/home/runner/rate2083/public/pages/index.html')
-})
-app.post('/api', (req, res) => {
-    teach(req, res)
-})
-
-app.listen(3000)
+app.listen()
